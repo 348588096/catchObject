2a30decee2757f00a7476b27622c7b9d4f79df36
348588096 <veznup@gmail.com>
2025-06-01 07:53:44 -0700

    Created a Jump Function and Movment
    -When space bar is pressed: following the law in physics it will complete a jump in a form of parabola
    -When a key is pressed Mario would move 4 units to the left
    When d key is pressed Mario would move 4 units to the right

--- Mario.java
+++ Mario.java
@@ -1,26 +1,55 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Mario here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Mario extends Actor
 {
-    public Mario()
+    private int velocityY = 0;
+    private int gravity = 1;
+    private boolean onGround = true;
+
+    public void Mario()
     {
         setImage("images/mario.png");
-        
     }
     
     public void act()
     {
-        movment();
+        moveLeftRight();
+        jump();
+        fall();
     }
-    
-    public void movment()
+
+    public void moveLeftRight()
     {
-        
+        if (Greenfoot.isKeyDown("a"))
+        {
+            setLocation(getX() - 4, getY());
+        }
+        if (Greenfoot.isKeyDown("d"))
+        {
+            setLocation(getX() + 4, getY());
+        }
+    }
+
+    public void jump()
+    {
+        if (Greenfoot.isKeyDown("space") && onGround)
+        {
+            velocityY = -15;   // Jump strength
+            onGround = false;
+        }
+    }
+
+    public void fall()
+    {
+        setLocation(getX(), getY() + velocityY);
+        velocityY += gravity;
+
+        if (getY() >= 300)  // Set this to your ground level
+        {
+            setLocation(getX(), 300);
+            velocityY = 0;
+            onGround = true;
+        }
     }
 }
